e61e78e34dfa5b29a1ce95db3b79b439f7c305a4
H02c4n <svtr.hozcan@gmail.com>
2022-09-07 12:42:41 +0200

    api implementation done

--- app.js
+++ app.js
@@ -1,12 +1,66 @@
-const req = new XMLHttpRequest();
+const addBtn = document.querySelector("#add-btn");
+const inputText = document.querySelector("#input-text");
+const textContent = document.querySelector("#text-content");
+var storyTelling = "";
 
-req.open("GET", "https://api.dictionaryapi.dev/api/v2/entries/en/" + word);
+if (textContent.textContent == "") {
+  document.querySelector(".row").style.display = "none";
+}
 
-req.send();
+addBtn.addEventListener("click", function () {
+  storyTelling = inputText.value;
+  //console.log(storyTelling);
+  setText(storyTelling);
+  if (textContent.textContent) {
+    document.querySelector(".row").style.display = "flex";
+  }
 
-req.addEventListener("load", function () {
-  const data = JSON.parse(this.responseText);
-  console.log(data);
-  console.log(data[0].meanings[0].definitions[0].definition);
-  console.log(data[0].meanings[0].definitions[0].example);
+  const span = document.querySelectorAll("#text-content>span");
+
+  span.forEach((s) => {
+    s.addEventListener("mouseover", function () {
+      const req = new XMLHttpRequest();
+
+      req.open(
+        "GET",
+        "https://api.dictionaryapi.dev/api/v2/entries/en/" + s.innerText
+      );
+
+      req.send();
+
+      req.addEventListener("load", function () {
+        const data = JSON.parse(this.responseText);
+
+        //console.log(span);
+        showDict(data);
+      });
+    });
+  });
+
+  console.log();
 });
+
+showDict = (data) => {
+  const dictionary = document.querySelector("#dictionary");
+  let html = `
+          <div class="card">
+            <ul class="list-group list-group-flush">
+              <li class="list-group-item">${data[0].meanings[0].definitions[0].definition}</li>
+              <li class="list-group-item">${data[0].meanings[0].definitions[0].example}</li>
+            </ul>
+          </div>
+  `;
+
+  dictionary.innerHTML = html;
+};
+
+setText = (text) => {
+  const textArr = text.split(" ");
+  let html = "";
+  for (let i = 0; i < textArr.length; i++) {
+    html += `
+    <span> ${textArr[i]} </span>
+    `;
+    textContent.innerHTML = html;
+  }
+};
